67369c8f924474fef4df5db20ebb9fe270fbd902
luw <luw@mysoft.com.cn>
2016-10-11 10:53:03 +0800

    增加模板

---
+++ environments/dev/common/config/main-local.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=10.5.23.15;dbname=data_config',
+            'username' => 'dev',
+            'password' => 'Mysoft123',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
